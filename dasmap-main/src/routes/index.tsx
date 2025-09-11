29a788426f8de344cd48bb0c86935b8d45d5aaac
fralo <francisnatividad333@gmail.com>
2025-09-11 08:45:11 +0800

    removed api import

--- dasmap-main/src/routes/index.tsx
+++ dasmap-main/src/routes/index.tsx
@@ -15,7 +15,6 @@ import { SidebarTrigger } from '@/components/ui/sidebar'
 import Header from '../components/main/Header'
 import { Rotate3d, Box } from "lucide-react"
 import Terms from "@/components/main/terms"
-import GetData from "@/components/main/api/main"
 
 
 interface RefStruct {
