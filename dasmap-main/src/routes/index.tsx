93368264e77c399bd7fb5e8f51143a2d245e4813
fralo <francisnatividad333@gmail.com>
2025-09-11 08:51:06 +0800

    fix click

--- dasmap-main/src/routes/index.tsx
+++ dasmap-main/src/routes/index.tsx
@@ -56,7 +56,6 @@ export default function App() {
 
   //Global Click Function
   function handleClick(name: string) {
-    GetData()
     if (activeBarangay === name && mapOn) {
       setActiveBarangay("")
       resetCamera()
