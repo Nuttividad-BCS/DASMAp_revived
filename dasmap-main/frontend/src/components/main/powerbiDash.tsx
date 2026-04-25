f8b74cc3ee9f06e455639675ec25b8a0ee96d124
fralo <francisnatividad333@gmail.com>
2026-04-25 13:22:28 +0800

    Reformat Dashboard

---
+++ dasmap-main/frontend/src/components/main/powerbiDash.tsx
@@ -0,0 +1,10 @@
+function PowerBiDash() {
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
+      <iframe title="DASMAP-PowerBi" width="100%" height="100%" src="https://app.powerbi.com/reportEmbed?reportId=c7e7bcbd-d5db-4ffd-aecb-50b8d37549e1&autoAuth=true&ctid=08195d68-0190-47b8-8344-b172f23ce9c5&navContentPaneEnabled=false&filterPaneEnabled=false" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+  );
+}
+
+export default PowerBiDash;
